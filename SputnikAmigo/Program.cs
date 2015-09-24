934e8b1a4f3841791a057dec1fb7f5cedc3add6d
KiberKrab <fdooch@ngs.ru>
2015-09-24 10:14:19 +0600

    Initial commit

---
+++ SputnikAmigo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SputnikAmigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Installing Amigo...");
+            Thread.Sleep(500);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + "%");
+                Thread.Sleep(50);
+
+            }
+            Console.WriteLine("Install success!");
+
+            Thread.Sleep(500);
+
+            Console.WriteLine("Installing Sputnik...");
+            Thread.Sleep(500);
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + "%");
+                Thread.Sleep(50);
+
+            }
+            Console.WriteLine("Install success!");
+        }
+    }
+}
